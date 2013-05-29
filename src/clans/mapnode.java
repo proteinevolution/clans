b78a3c9b47b6b0af964049919a77f7a588e34a96
Klaus O. Kopec <klaus.kopec@tuebingen.mpg.de>
2013-05-29 13:53:26 +0200

    replaced java.lang.reflect.Array.getLength(x) with x.length and fixed all warnings

--- src/clans/mapnode.java
+++ src/clans/mapnode.java
@@ -1,54 +1,45 @@
-/*
- * mapnode.java
- *
- * Created on June 20, 2006, 3:15 PM
- */
 package clans;
-/**
- *
- * @author  tancred
- */
+
 public class mapnode {
     
-    /** Creates a new instance of mapnode */
-    public mapnode() {
-    }
-    
-    String level="undefined";
-    String info="undefined";
-    mapnode[] child=null;
-    String leaf="undefined";
-    
-    //--------------------------------------------------------------------------
-    
+    String level = "undefined";
+    String info = "undefined";
+    mapnode[] child = null;
+    String leaf = "undefined";
+
+    /**
+     * see if this node contains a specific child
+     * 
+     * @param check
+     * @return
+     */
     public int haschild(mapnode check){
-        //see if this node contains a specific child
         if(child==null){
             return -1;
         }
-        for(int i=java.lang.reflect.Array.getLength(child);--i>=0;){
+
+        for (int i = child.length; --i >= 0;) {
             if(check==child[i]){
                 return i;
             }
-        }//end for i
+        }
         return -1;
     }
     
-    //--------------------------------------------------------------------------
-    
+    /**
+     * 
+     * @param addchild
+     */
     public void add(mapnode addchild){
         if(child==null){
             child=new mapnode[1];
             child[0]=addchild;
         }else{
-            int size=java.lang.reflect.Array.getLength(child);
+            int size = child.length;
             mapnode[] tmp=child;
             child=new mapnode[size+1];
             System.arraycopy(tmp,0,child,0,size);
             child[size]=addchild;
         }
-    }
-    
-    //--------------------------------------------------------------------------
-    
+    }   
 }
