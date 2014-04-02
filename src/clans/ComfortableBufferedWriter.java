0fda3415cc8d6bb00d23b364cab1b481a53ce36a
Klaus O. Kopec <klaus.kopec@tuebingen.mpg.de>
2014-04-02 13:12:17 +0200

    added new ComfortableBufferedWriter class which I forgot in last commit

---
+++ src/clans/ComfortableBufferedWriter.java
@@ -0,0 +1,34 @@
+package clans;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * A slightly more comfortable BufferedWriter to make saving runs with {@code ClusterData.saverun} easier to code.
+ */
+public class ComfortableBufferedWriter extends BufferedWriter {
+
+	public ComfortableBufferedWriter(Writer out) {
+		super(out);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ComfortableBufferedWriter(Writer out, int sz) {
+		super(out, sz);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void print(Object in) throws IOException {
+		write(in.toString());
+	}
+	
+	public void println() throws IOException {
+		write("\n");
+	}
+	
+	public void println(Object in) throws IOException {
+		write(in.toString() + "\n");
+	}
+
+}
